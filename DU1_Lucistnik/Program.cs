5600f7d31366b0ea1cd153088a406fc85f94b269
Lucie B <bizova.lucie@gmail.com>
2025-04-23 13:09:04 +0200

    Zobrazení aktuálního stavu šípů a textové menu pro uživatele

--- DU1_Lucistnik/Program.cs
+++ DU1_Lucistnik/Program.cs
@@ -8,6 +8,12 @@ class Program
     {
         //1x lučištník pomocí konstruktoru z třídy Lučištník 
         Lucistnik william = new Lucistnik("William", 25);
-        Console.WriteLine ($"Lučištník {william.Name} má v toulci {william.NumberOfArrows} šípů. ");
+        //Console.WriteLine ($"Lučištník {william.Name} má v toulci {william.NumberOfArrows} šípů. "); //1. test
+        william.DisplayStatus(); //aktuální stav šípů pomocí metody DisplayStatus
+        Console.WriteLine ("Lučistníku přeješ si : ");
+        Console.WriteLine ("Vystřelit šíp (stiskni 1) nebo ");
+        Console.WriteLine ("Přidat šípy do toulce (stikni 2) nebo ");
+        Console.WriteLine ("Ukončit střelbu (stiskni 3). ");
+        Console.ReadLine ();
     }
 }
